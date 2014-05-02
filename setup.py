cee04c004b418918dacc39c2d60c342d2e64f5e7
DavideCanton <davide.canton5@gmail.com>
2014-05-02 18:05:03 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup, Extension
+
+module1 = Extension('pyBitSet',
+                    define_macros = [('MAJOR_VERSION', '1'),
+                                     ('MINOR_VERSION', '0')],
+                    include_dirs = ['C:\Python34\include'],
+                    sources = ['pybitset.c'])
+
+setup (name = 'PyBitSet',
+       version = '1.0',
+       description = 'PyBitSet module',
+       author = 'Davide Canton',
+       author_email = 'davide.canton5@gmail.com',
+       long_description = '''
+PyBitSet is my first C-extension for Python.
+
+This module exports a class, PyBitSet, that represents a bit set, i.e. a fixed-size container for integers.
+''',
+       ext_modules = [module1])
