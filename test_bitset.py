5401f07c92099118a0c73b1f66981ceceb2e7e71
DavideCanton <davide.canton5@gmail.com>
2014-07-18 11:17:54 +0200

    Added elems() method.

--- test_bitset.py
+++ test_bitset.py
@@ -9,17 +9,16 @@ if __name__ == "__main__":
     init_val = {random.randint(0, length - 1) for _ in range(5)}
     bitset = PyBitSet(length, init_val)
     print(init_val)
-    # bin_str = bitset.to_bin_str()
-    # print(bin_str)
+    bin_str = bitset.to_bin_str()
+    print(bin_str)
     print(bitset)
+    print(set(bitset.elems()))
+
+    new_val = {random.randint(0, length - 1) for _ in range(5)}
+    print(new_val)
+    bitset.update(new_val)
+    print(bitset)
+    print(bitset.to_bin_str())
     print(set(it.compress(range(length), bitset)))
 
-    # new_val = {random.randint(0, length - 1) for _ in range(5)}
-    #print(new_val)
-    n = random.randint(0, 20)
-    s = [ord(c) - ord("0") for c in reversed(bin(n)[2:])]
-    print(set(it.compress(range(length), s)))
-    bitset.update(n)
-    print(bitset)
-    #print(bitset.to_bin_str())
-    print(set(it.compress(range(length), bitset)))
+    assert set(it.compress(range(length), bitset)) == set(bitset.elems())
